b2f5fa7107050cb204ee984babbb8c19f7c43a2b
Vladyslav Prytula <vladprytula.github@gmail.com>
2025-02-17 20:05:56 +0200

    feat: sdk

--- src/components/pages/CryptoWalletPage/CryptoWalletPage.tsx
+++ src/components/pages/CryptoWalletPage/CryptoWalletPage.tsx
@@ -1,33 +1,12 @@
 import { useEffect } from 'react';
-import {
-  XpaidWalletSdk,
-  SdkConfig,
-  Environment
-} from '../../../../../../../Monorepo/work/xpaid-wallet/packages/sdk/src';
-import { api } from '@/lib/axios';
-import { ApiKey } from '@/constants';
+import { initSdk } from '@/lib/sdk';
+
+const customerId = '1a963cad-a375-42b8-bc2a-428645d2181e'
+const iban = 'test'
 
 export const CryptoWalletPage = () => {
   useEffect(() => {
-
-    const initSdk = async () => {
-      const customerId = '1a963cad-a375-42b8-bc2a-428645d2181e'
-      const iban = 'test'
-
-      const response = await api.get(`/tokens/hash/${customerId}/${iban}`, {
-        headers: { 'X-API-KEY': ApiKey },
-      })
-
-      const config: SdkConfig = {
-        environment: Environment.Development,
-        containerId: 'xpaid-wallet-container',
-        hashToken: response.data
-      };
-
-      XpaidWalletSdk.initialize(config);
-    }
-
-    initSdk()
+    initSdk(customerId, iban)
   }, []);
 
   return (
---
+++ src/lib/sdk.ts
@@ -0,0 +1,17 @@
+import { ApiKey } from "@/constants";
+import { api } from "./axios";
+import { Environment, SdkConfig, XpaidWalletSdk } from "xpaid-wallet-sdk";
+
+export async function initSdk(customerId: string, iban: string) {
+  const response = await api.get(`/tokens/hash/${customerId}/${iban}`, {
+    headers: { 'X-API-KEY': ApiKey },
+  })
+
+  const config: SdkConfig = {
+    environment: Environment.Staging,
+    containerId: 'xpaid-wallet-container',
+    hashToken: response.data
+  };
+
+  XpaidWalletSdk.initialize(config);
+}
