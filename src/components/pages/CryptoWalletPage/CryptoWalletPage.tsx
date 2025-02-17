7e683d63422f035cdfced2321c20517f27b3534c
Vladyslav Prytula <vladprytula.github@gmail.com>
2025-02-17 13:47:30 +0200

    feat: update sdk, change sdk config

--- src/components/pages/CryptoWalletPage/CryptoWalletPage.tsx
+++ src/components/pages/CryptoWalletPage/CryptoWalletPage.tsx
@@ -4,34 +4,32 @@ import {
   Message,
   SdkConfig,
   Environment
-} from 'xpaid-wallet-sdk';
+} from '../../../../../../../Monorepo/work/xpaid-wallet/packages/sdk/src';
+
 export const CryptoWalletPage = () => {
   useEffect(() => {
     const config: SdkConfig = {
-      containerId: 'xpaid-wallet-container',
-      token: '',
       environment: Environment.Staging,
-      width: '100%',
-      height: '100%',
-      colorScheme: 'light'
+      containerId: 'xpaid-wallet-container',
+      customerAccount: {
+        customerId: '1a963cad-a375-42b8-bc2a-428645d2181e',
+        iban: 'test'
+      },
     };
 
     XpaidWalletSdk.initialize(config);
 
-    const handleTransferCreated = (payload: unknown) => {
-      console.log('Transfer Created:', payload);
-    };
-    XpaidWalletSdk.subscribe(Message.TransferCreated, handleTransferCreated);
 
-    return () => {
-      XpaidWalletSdk.unsubscribe(Message.TransferCreated);
-      XpaidWalletSdk.destroy();
-    };
+    XpaidWalletSdk.subscribe(Message.TransferCreated, (payload) => {
+      console.log('Transfer Created:', payload);
+    });
+
+    return XpaidWalletSdk.destroy
   }, []);
 
   return (
-    <div className="flex flex-col ">
-      <div id="xpaid-wallet-container" className="w-full h-[91vh]" />
+    <div className="w-full h-full flex flex-col">
+      <div id="xpaid-wallet-container" className="w-full h-full" />
     </div>
   );
 };
