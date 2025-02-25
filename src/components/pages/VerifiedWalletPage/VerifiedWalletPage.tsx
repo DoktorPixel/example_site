cbe319e4f9b3f536b39243122619741313929d54
Vladyslav Prytula <vladprytula.github@gmail.com>
2025-02-25 17:36:57 +0200

    feat: change alert

--- src/components/pages/VerifiedWalletPage/VerifiedWalletPage.tsx
+++ src/components/pages/VerifiedWalletPage/VerifiedWalletPage.tsx
@@ -16,24 +16,25 @@ const customerId = '485bb67b-a9a2-4e0c-8a17-c1baa02dddd6'
 const iban = 'test'
 
 export const VerifiedWalletPage = () => {
-  const [open, setOpen] = useState(false)
-
+  const [open, setOpen] = useState(false);
+  const [amount, setAmount] = useState<number | null>(null);
 
   useEffect(() => {
-    initSdk(customerId, iban)
+    initSdk(customerId, iban);
 
-    XpaidWalletSdk.subscribe(Message.TransferCreated, () => {
-      setOpen(true)
-    })
+    XpaidWalletSdk.subscribe(Message.TransferCreated, ({ amount }) => {
+      setOpen(true);
+      setAmount(amount);
+    });
 
     return () => {
-      XpaidWalletSdk.destroy()
-    }
+      XpaidWalletSdk.destroy();
+    };
   }, []);
 
   const handleContinue = () => {
-    XpaidWalletSdk.publish(Message.TransferConfirmed, {})
-  }
+    XpaidWalletSdk.publish(Message.TransferConfirmed, {});
+  };
 
   return (
     <div className="w-full h-full flex flex-col">
@@ -41,15 +42,20 @@ export const VerifiedWalletPage = () => {
       <AlertDialog open={open} onOpenChange={setOpen}>
         <AlertDialogContent>
           <AlertDialogHeader>
-            <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
+            <AlertDialogTitle>Confirm Transaction</AlertDialogTitle>
             <AlertDialogDescription>
-              This action cannot be undone. This will permanently delete your account
-              and remove your data from our servers.
+              {amount !== null ? (
+                <>
+                  Are you sure you want to confirm the transfer of {amount}?
+                </>
+              ) : (
+                "Are you sure you want to confirm this transaction?"
+              )}
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
             <AlertDialogCancel>Cancel</AlertDialogCancel>
-            <AlertDialogAction onClick={handleContinue}>Continue</AlertDialogAction>
+            <AlertDialogAction onClick={handleContinue}>Confirm</AlertDialogAction>
           </AlertDialogFooter>
         </AlertDialogContent>
       </AlertDialog>
