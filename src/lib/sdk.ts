2fa7363fddbfa1729be37f32a7584a8ea5d2d142
Vladyslav Prytula <vladprytula.github@gmail.com>
2025-02-27 14:08:08 +0200

    feat: update sdk

--- src/lib/sdk.ts
+++ src/lib/sdk.ts
@@ -8,7 +8,7 @@ export async function initSdk(customerId: string, iban: string) {
     headers: { 'X-API-KEY': ApiKey },
   })
   const config: SdkConfig = {
-    environment: Environment.Development,
+    environment: Environment.Staging,
     containerId: 'xpaid-wallet-container',
     hashToken: response.data.data,
   };
