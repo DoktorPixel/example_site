aaf5ff326f84012d6dfca468051f183e6721f5d8
Vladyslav Prytula <vladprytula.github@gmail.com>
2025-04-17 19:55:35 +0300

    fix

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,6 +1,7 @@
+import { isDev } from '@/constants'
 import axios from 'axios'
 
-export const baseURL = import.meta.env.DEV ? 'https://devwalletapi.xpaid.org/api/v1' : 'https://stagewalletapi.xpaid.org/api/v1'
+export const baseURL = isDev? 'https://devwalletapi.xpaid.org/api/v1' : 'https://stagewalletapi.xpaid.org/api/v1'
 
 export const api = axios.create({
   baseURL,
