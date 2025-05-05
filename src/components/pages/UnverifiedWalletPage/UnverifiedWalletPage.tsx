7d6999aa43d62cc4a9adb4c63e8f95e99d9bb76a
Vladyslav Prytula <vladprytula.github@gmail.com>
2025-05-05 17:35:35 +0300

    fix

--- src/components/pages/UnverifiedWalletPage/UnverifiedWalletPage.tsx
+++ src/components/pages/UnverifiedWalletPage/UnverifiedWalletPage.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 import { initSdk } from '@/lib/sdk';
 
-const customerId = '885a785-307e-4e3e-bd02-b0f1097ed565'
+const customerId = '7885a785-307e-4e3e-bd02-b0f1097ed565'
 const iban = 'unverified'
 const accountId = 'bogdan-test-unique'
 
